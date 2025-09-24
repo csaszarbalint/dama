4f6422037751f6331a37505ebfcc4268be05fdfb
csaszarbalint <csaszar.balint@hbsz.edu.hu>
2025-09-24 12:56:53 +0200

    might need to make CheckersBoard a singelton

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ class Piece{
     constructor(position){
         this.pos = position
         this.div;
+        this.moveVectors = []
+    }
+
+    moveBy(vec){
+        this.pos = this.pos.add(vec)
     }
 }
 
@@ -37,19 +42,30 @@ class CheckersBoard{
         this.WHITE_CELL_STYLE = "cell_white"
         this.BLACK_PIECE_STYLE = "piece_black"
         this.WHITE_PIECE_STYLE = "piece_white"
+        this.BLACK_MOVE_VECTORS = [
+            new Vec2(-1,1),
+            new Vec2(1,1)
+        ]
+        this.WHITE_MOVE_VECTORS = [
+            new Vec2(-1,-1),
+            new Vec2(1,-1)
+        ]
         
         this.parentNode = parentNode
 
         this.cells = new Map()
+        this.pieceDivLookup = new Map()
         this.whitePieces = []
         this.blackPieces = []
 
         this.initialize()
-    }
 
-    #onPieceClicked(event){
-        const piece = event.target.parentNode
+        this.render()
 
+        this.blackPieces[1].moveBy(new Vec2(0,1))
+
+        debugger
+        this.render()
     }
 
     initialize() {
@@ -57,23 +73,6 @@ class CheckersBoard{
         this.parentNode.setAttribute('style',
              'grid-template-columns: repeat(' + this.HEIGHT + ', 1fr);' +
              'grid-template-rows: repeat(' + this.WIDTH + ', 1fr);');
-
-        //generating cells
-        for(let i = 0; i < this.HEIGHT; i++){
-            for(let j = 0; j < this.WIDTH; j++){
-                const cell = document.createElement('div')
-                const pos = new Vec2(j, this.HEIGHT - i - 1)
-
-                //probably not a great idea
-                this.cells.set(pos.encode, cell)
-                cell.pos = pos 
-
-                cell.classList.add('cell')
-                cell.classList.add((i+j) % 2 === 1 ? this.BLACK_CELL_STYLE : this.WHITE_CELL_STYLE)
-
-                this.parentNode.appendChild(cell)
-            }
-        }
 
         //placeing pieces 
         const pattern = [
@@ -90,22 +89,54 @@ class CheckersBoard{
 
             blackPiece.div = document.createElement('div')
             blackPiece.div.classList.add("piece", this.BLACK_PIECE_STYLE)
-            blackPiece.div.addEventListener('click', this.#onPieceClicked)
-            this.cells.get(blackPiece.pos.encode).appendChild(blackPiece.div)
+            blackPiece.div.addEventListener('click', this.onPieceClicked)
+            blackPiece.div.piece = blackPiece
 
             whitePiece.div = document.createElement('div')
             whitePiece.div.classList.add("piece", this.WHITE_PIECE_STYLE)
-            whitePiece.div.addEventListener('click', this.#onPieceClicked)
-            this.cells.get(whitePiece.pos.encode).appendChild(whitePiece.div)
+            whitePiece.div.addEventListener('click', this.onPieceClicked)
+            whitePiece.div.piece = whitePiece 
+
+            blackPiece.moveVectors = this.BLACK_MOVE_VECTORS
+            whitePiece.moveVectors = this.WHITE_MOVE_VECTORS
 
             this.blackPieces.push(blackPiece)
             this.whitePieces.push(whitePiece)
 
             currentPos = currentPos.add(pattern[offset])
-            if(currentPos.x > this.SIZE.x || currentPos.y > this.SIZE.y) break; //exit condition
+            if(currentPos.x > this.SIZE.x || currentPos.y > this.SIZE.y) break;  
 
             if(offset >= pattern.length - 1) offset = -1
             offset++;
+        }
+    }
+
+    generateCells(){
+        this.cells = new Map()
+        for(let i = 0; i < this.HEIGHT; i++){
+            for(let j = 0; j < this.WIDTH; j++){
+                const cell = document.createElement('div')
+                const pos = new Vec2(j, this.HEIGHT - i - 1)
+
+                this.cells.set(pos.encode, cell)
+                cell.pos = pos 
+
+                cell.classList.add('cell')
+                cell.classList.add((i+j) % 2 === 1 ? this.BLACK_CELL_STYLE : this.WHITE_CELL_STYLE)
+
+                this.parentNode.appendChild(cell)
+            }
+        }
+
+    }
+
+    render(){
+        while (this.parentNode.firstChild) {
+            this.parentNode.removeChild(this.parentNode.lastChild);
+        }
+        this.generateCells()
+        for(const piece of this.blackPieces.concat(this.whitePieces)){
+            this.cells.get(piece.pos.encode).appendChild(piece.div)
         }
     }
 }
