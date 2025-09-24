df7b0b68e3f50ec0ab99b785df2d3049d76fd943
csaszy <csaszy2006@gmail.com>
2025-09-24 21:23:57 +0200

    looks good , markers don't work jet

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ class Vec2{
         return new Vec2(this.x + vec.x, this.y + vec.y) 
     }
 
+    subtract(vec){
+        return new Vec2(this.x - vec.x, this.y - vec.y) 
+    }
+
     multiply(n){
         return new Vec2(this.x * n, this.y * n)
     }
@@ -21,15 +25,41 @@ class Vec2{
     }
 }
 
-class Piece{
+class Piece extends HTMLElement{
     constructor(position){
+        super()
+
+        this.onPositionSet = () => {};
+        
         this.pos = position
-        this.div;
         this.moveVectors = []
+        this.owner; //group id
     }
 
     moveBy(vec){
         this.pos = this.pos.add(vec)
+    }
+
+    moveTo(vec){
+        this.pos = vec 
+    }
+
+    set pos(vec){
+        this.position = vec
+        this.style.gridRow = 8 - this.position.y //TODO: remove magic number 
+        this.style.gridColumn = this.position.x + 1
+        this.onPositionSet(this.position)
+    }
+
+    get pos(){
+        return this.position
+    }
+}
+
+class Move{
+    constructor(position, action){
+        this.pos = position
+        this.action = action
     }
 }
 
@@ -50,36 +80,44 @@ class CheckersBoard{
             new Vec2(-1,-1),
             new Vec2(1,-1)
         ]
-        
-        this.parentNode = parentNode
-
-        this.cells = new Map()
-        this.pieceDivLookup = new Map()
-        this.whitePieces = []
-        this.blackPieces = []
-
-        this.initialize()
-
-        this.render()
-
-        this.blackPieces[1].moveBy(new Vec2(0,1))
-
-        debugger
-        this.render()
-    }
-
-    initialize() {
-        //setting table dimension
-        this.parentNode.setAttribute('style',
-             'grid-template-columns: repeat(' + this.HEIGHT + ', 1fr);' +
-             'grid-template-rows: repeat(' + this.WIDTH + ', 1fr);');
-
-        //placeing pieces 
-        const pattern = [
+        this.STARTING_PATTERN = [
             new Vec2(0,2), //2 up
             new Vec2(1,-1), //1 down, 1 left 
             new Vec2(1,-1), //1 down, 1 left 
-        ];
+        ]
+        
+        this.parentNode = parentNode
+
+        this.foreground = document.createElement('div')
+        this.background = document.createElement('div')
+
+        this.cells = new Map()
+        this.pieceDivLookup = new Map()
+
+        this.pieces = []
+        this.validMoves = []
+
+        this.initialize()
+
+    }
+
+    initialize() {
+        customElements.define("piece-div", Piece)
+
+        this.foreground.classList.add('layer')
+        this.foreground.setAttribute('style',
+             'grid-template-columns: repeat(' + this.HEIGHT + ', 1fr);' +
+             'grid-template-rows: repeat(' + this.WIDTH + ', 1fr);');
+
+        this.background.classList.add('layer')
+        this.background.setAttribute('style',
+             'grid-template-columns: repeat(' + this.HEIGHT + ', 1fr);' +
+             'grid-template-rows: repeat(' + this.WIDTH + ', 1fr);');
+
+        this.parentNode.appendChild(this.background)
+        this.parentNode.appendChild(this.foreground)
+
+        this.generateCells(this.background)
 
         let currentPos = new Vec2(0,0);
         let offset = 0;
@@ -87,31 +125,37 @@ class CheckersBoard{
             const blackPiece = new Piece(currentPos)
             const whitePiece = new Piece(this.SIZE.add(currentPos.multiply(-1)))
 
-            blackPiece.div = document.createElement('div')
-            blackPiece.div.classList.add("piece", this.BLACK_PIECE_STYLE)
-            blackPiece.div.addEventListener('click', this.onPieceClicked)
-            blackPiece.div.piece = blackPiece
-
-            whitePiece.div = document.createElement('div')
-            whitePiece.div.classList.add("piece", this.WHITE_PIECE_STYLE)
-            whitePiece.div.addEventListener('click', this.onPieceClicked)
-            whitePiece.div.piece = whitePiece 
+            blackPiece.classList.add("piece", this.BLACK_PIECE_STYLE)
+            blackPiece.addEventListener('click', (e) => this.onPieceClicked(e))
+            whitePiece.classList.add("piece", this.WHITE_PIECE_STYLE)
+            whitePiece.addEventListener('click', (e) => this.onPieceClicked(e))
 
             blackPiece.moveVectors = this.BLACK_MOVE_VECTORS
             whitePiece.moveVectors = this.WHITE_MOVE_VECTORS
 
-            this.blackPieces.push(blackPiece)
-            this.whitePieces.push(whitePiece)
+            blackPiece.owner = "black" //TODO: remove these magic values
+            whitePiece.owner = "white"
+            
+            this.pieces.push(blackPiece)
+            this.pieces.push(whitePiece)
 
-            currentPos = currentPos.add(pattern[offset])
+            this.foreground.appendChild(blackPiece)
+            this.foreground.appendChild(whitePiece)
+
+            currentPos = currentPos.add(this.STARTING_PATTERN[offset++])
             if(currentPos.x > this.SIZE.x || currentPos.y > this.SIZE.y) break;  
 
-            if(offset >= pattern.length - 1) offset = -1
-            offset++;
+            if(offset >= this.STARTING_PATTERN.length) offset = 0
         }
     }
 
-    generateCells(){
+    removePiece(piece){
+        this.pieces = this.pieces.filter(e => e != piece)
+        this.foreground.removeChild(piece)
+    }
+
+    generateCells(node){
+        node.innerHTML = ''
         this.cells = new Map()
         for(let i = 0; i < this.HEIGHT; i++){
             for(let j = 0; j < this.WIDTH; j++){
@@ -124,19 +168,100 @@ class CheckersBoard{
                 cell.classList.add('cell')
                 cell.classList.add((i+j) % 2 === 1 ? this.BLACK_CELL_STYLE : this.WHITE_CELL_STYLE)
 
-                this.parentNode.appendChild(cell)
+                node.appendChild(cell)
             }
         }
 
     }
 
-    render(){
-        while (this.parentNode.firstChild) {
-            this.parentNode.removeChild(this.parentNode.lastChild);
+    /**
+     * 
+     * @param {Vec2} vec 
+     * @returns 
+     * null- if position is outside of play area
+     * Piece - if there is a piece under the position  
+     * Cell - if there is no peace under the position
+     */
+    whatIsAtPosition(vec){
+        const dV = this.SIZE.subtract(vec)
+
+        if(dV.x > this.SIZE.x || dV.x < 0 ){
+            console.warn("Invalid x position")
+            console.dir(vec)
+            return null
         }
-        this.generateCells()
-        for(const piece of this.blackPieces.concat(this.whitePieces)){
-            this.cells.get(piece.pos.encode).appendChild(piece.div)
+        
+        if(dV.y > this.SIZE.y || dV.y < 0 ){
+            console.warn("Invalid y position")
+            console.dir(vec)
+            return null
+        }
+
+        for(const piece of this.pieces){
+            if(piece.pos.encode == vec.encode) return piece
+        }
+
+        return this.cells.get(vec.encode)
+    }
+
+    getValidPositions(piece){
+        let vp = []
+        for(const vec of piece.moveVectors){
+            const posToCheck = piece.pos.add(vec)
+
+            const objAtPos = this.whatIsAtPosition(posToCheck)
+
+            //outside
+            if(!objAtPos) continue
+
+            if(objAtPos instanceof Piece){
+                //players own piece
+                if(objAtPos.owner === piece.owner) continue  
+
+                //opponents piece
+                const posBehindOpponentPiece = posToCheck.add(vec)
+                const objBehindOpponentPiece = this.whatIsAtPosition(posBehindOpponentPiece)
+
+                if(objBehindOpponentPiece instanceof Piece || objBehindOpponentPiece == null) return
+                vp.push(new Move(posBehindOpponentPiece, () => {
+                    this.removePiece(objAtPos);
+                    piece.moveTo(posBehindOpponentPiece)
+                }))
+                continue 
+            }
+
+            //nothing there
+            vp.push(new Move(posToCheck, () => {piece.moveTo(posToCheck)}))
+        }
+        return vp
+    }
+
+    createTempPiece(vec){
+        const piece = new Piece(vec)
+
+        piece.classList.add('piece')
+        piece.classList.add('marker')
+        piece.addEventListener('click', (e) => {this.onMarkerClicked(e)})
+
+        return piece
+    }
+
+    onPieceClicked(e){
+        const piece = e.target
+        this.generateCells(this.background) 
+
+        this.validMoves = this.getValidPositions(piece) 
+        for(let e of this.validMoves){
+            const marker = this.createTempPiece(e.pos)
+            this.foreground.appendChild(marker)
+        }
+    }
+
+    onMarkerClicked(e){
+        if(this.validMoves == []) return
+        for(const move of this.validMoves){
+            if(move.pos.encode === e.target.pos.encode)
+                move.action()
         }
     }
 }
@@ -192,5 +317,5 @@ function resetAll() {
 
 
 document.body.onload = () => {
-    let a = new CheckersBoard(document.querySelector('main'))
+    new CheckersBoard(document.querySelector('main'))
 }
